52046660f7da4f29b31d1a39e14b30e34207ec45
another-greg <164979512+another-greg@users.noreply.github.com>
2026-05-05 12:06:10 -0700

    Update MAE170_Lab6_Speaker.ino
    changed Tone to use a delayMicroseconds for improved precision with ~1ms tone durations. On R4 minima, the tone is unable to accurately hit a number of periods equal to 1ms

--- 06 Acoustics/MAE170_Lab6_Speaker.ino
+++ 06 Acoustics/MAE170_Lab6_Speaker.ino
@@ -1,4 +1,4 @@
-float Tmillis=0, freq=0;
+float Tmicros=0, freq=0;
 String freq_str=" "; // initialize frequency string variable
 
 void setup() {
@@ -8,7 +8,11 @@ void loop() {
    while (Serial.available() > 0) { // loop while there are strings in read buffer
     freq_str = Serial.readString(); // read string
     freq=freq_str.toFloat(); // convert string to float type
-    Tmillis=1*1000*5/freq; // calculate duration for 5 cycle pulse of given frequency
-    tone(9,freq,Tmillis); // generate tone at given frequency for above duration on pin 9
+    Tmicros=1*1e6*5/freq; // calculate duration for 5 cycle pulse of given frequency in microseconds
+                          // we use microseconds to ensure we have sufficient precision at ~1ms time scales
+    tone(9,freq); // generate tone at given frequency on pin 9
+    delayMicroseconds(Tmicros); // let tone run for desired amount of time. Need to use delayMicroseconds to ensure sufficient
+                                // precision at ~1ms scale
+    noTone(9); // stop the tone on pin 9
    }
 }
