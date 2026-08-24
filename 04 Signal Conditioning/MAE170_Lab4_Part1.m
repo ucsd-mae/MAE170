e21ad01577fb5b34794b9fc8d77153ba588c389a
Greg H <grhainline@ucsd.edu>
2026-08-24 15:40:20 -0700

    updated lab4 for pico

--- 04 Signal Conditioning/MAE170_Lab4_Part1.m
+++ 04 Signal Conditioning/MAE170_Lab4_Part1.m
@@ -4,8 +4,8 @@ clc;
  
 %% Parameters to set
 T = 2; % Total sampling time in seconds
-fs = 1000; % sampling frequency target in Hz
-cutFreq = 1; % insert your calculated cutoff frequency here
+fs = 5000; % sampling frequency target in Hz
+cutFreq = 1/(2*pi* 1e-6 *10.085e3); % insert your calculated cutoff frequency here
 f_min=1; % minimum frequency to be characterized in Hz
 f_max=2*cutFreq; % maximum frequency to be characterized in Hz
 f_step=1; % frequency step in Hz
@@ -44,13 +44,13 @@ for j=1:length(f_vec) % loop over each frequency to be tested
     % THIS SECTION IS FOR EXTRA CREDIT - uncomment for extra credit section
     % open object for your signal generator arduino
     % this segment sets the frequency of the generated signal
-    % s_gen = serialport('COM11',115200); % opens serial connect at specified baud rate
+    % s_gen = serialport('COMY',115200); % opens serial connect at specified baud rate
     % pause(5); % pause for 5 seconds while the serial object is opened
     % writeline(s_gen,'%s',int2str(f_vec(j))); % write the signal frequency
     % pause(2);
     % -------------------------------------
   
-    writeline(v,"APPLy:SIN " + int2str(f_vec(j)) + ",2,1.25"); % sending over a wave
+    writeline(v,"APPLy:SIN " + int2str(f_vec(j)) + ",1,0.75"); % sending over a wave
 
         
     flag=0; %set flag for timer
@@ -59,8 +59,8 @@ for j=1:length(f_vec) % loop over each frequency to be tested
     timer=0; % initialize timer
     L=T*fs*2; % oversized vector length
     time=zeros(L,1); % initialize time vector
-    A=zeros(L,1); % initialize amplitude vector
-    A_ref=zeros(L,1);
+    voltage=zeros(L,1); % initialize amplitude vector
+    voltage_ref=zeros(L,1);
     waittime=.5; %set initial wait time before sampling in seconds
     t=0; % initialize time variable
     ind=0; % initialize index variable
@@ -69,40 +69,68 @@ for j=1:length(f_vec) % loop over each frequency to be tested
     a_ref=0;
     dump=''; % initialize text dump variable
     out=''; % initialize serial output string variable
+    t0 = [];
     tic; % start timer
     
-    while toc<waittime % read and dump serial data until wait time is reached
-        dump=readline(s_read);
-    end
+    % Wait initial wiattime before sampling to allow function generator to
+    % setup
+    pause(waittime);
+    flush(s_read);
+
+    readline(s_read); % read 1 line to clear any half-read lines
     
-    while flag==0
-        out=char(readline(s_read)); % reading the serial port
-        ind_ref=find(out==',',1); % find index of comma
-        ind=find(out==';',1); % find index of semi-colon
-        a_ref=str2double(out(1:ind_ref-1)); % get amplitude of output signal
-        % get amplitude of reference signal
-        a=str2double(strtrim(out(ind_ref+1:ind-1))); 
-        t=str2double(strtrim(out(ind+1:end)))/1E6; % get time
-        % condition to take sample at set sampling rate
-        if (t-timer)>dt_set 
-            time(i) = t; % establishing time steps for sampling frequency
-            A(i)=a; % add signal amplitude to signal amplitude vector
-            A_ref(i)=a_ref; % add reference amplitude to vector
-            timer=time(i);
-            i=i+1;
-            % condition to end loop when end time is reached
-            if t>(T+time(1))
-                flag=1;
+    leftover = ''; % create placeholder for any unread partial lines
+    tic; % re-start timer to use for plot throttling
+    while flag == 0
+        nbytes = s_read.NumBytesAvailable;
+        if nbytes == 0
+            continue % nothing new yet, check again
+        end
+ 
+        raw = [leftover, read(s_read, nbytes, "char")]; % char array of everything available
+ 
+        lastNL = find(raw == newline, 1, 'last');
+        if isempty(lastNL)
+            leftover = raw; % no complete line yet, keep waiting for more
+            continue
+        end
+        leftover = raw(lastNL+1:end);   % save any partial trailing line for next pass
+        chunk = raw(1:lastNL);           % only complete lines
+ 
+        % parse every "adc, time_us" line in the chunk at once
+        vals = sscanf(chunk, '%f, %f, %f\n');
+        vals = vals(1:2*floor(numel(vals)/3)); % drop a stray unpaired value, if any
+        Aref_all = vals (1:3:end);
+        A_all = vals(2:3:end);
+        t_all = vals(3:3:end) / 1E6; % convert microseconds to seconds
+ 
+        if isempty(t0)
+            t0 = t_all(1);
+        end
+ 
+        % keep only samples spaced by at least dt_set 
+        for k = 1:numel(t_all)
+            if (t_all(k) - timer) > dt_set
+                time(i) = t_all(k) - t0; % time relative to first sample
+                voltage(i) = A_all(k) * 3.3/(2^10-1);
+                voltage_ref(i) = Aref_all(k) * 3.3/(2^10 - 1); % convert to full scale voltage
+                timer = t_all(k);
+                i = i + 1;
+ 
+                if t_all(k) > (T + t0) % condition to end loop when end time is reached
+                    flag = 1;
+                    break
+                end
             end
         end
     end
         
     reps=i-1; % get number of samples acquired
-    time = time(1:reps)-time(1); % setup a vector for time
-    % convert serial amplitude to voltage
-    voltage = 5/1023*A(1:reps); 
-    % convert serial amplitude to voltage for reference signal
-    voltage_ref = 5/1023*A_ref(1:reps); 
+    time = time(1:reps)-time(1); % trim and zero time vector
+    % trim excess vector
+    voltage = voltage(1:reps); 
+    % trim excess vector
+    voltage_ref = voltage_ref(1:reps); 
     % find the average time interval between samples
     dt_avg = mean(diff(time)); 
     fs_avg=1/dt_avg; % calculate the average sampling frequency from dt_avg
@@ -110,7 +138,7 @@ for j=1:length(f_vec) % loop over each frequency to be tested
     
     %% Create plot
     figure(01); % setup figure 01
-% plot time vs. voltage for output signal
+    % plot time vs. voltage for output signal
     subplot(221)
     plot(time, voltage(1:reps),'-o','LineWidth',2,'MarkerSize',4);
     xlabel('time (s)'); % x-axis label name
@@ -177,10 +205,11 @@ for j=1:length(f_vec) % loop over each frequency to be tested
     ylabel('|FT|_{max}/|FT|_{max,ref}'); % y-axis label name
     title('Transfer Function'); % set title as sampling rate
     xlim([f_min f_max]);
+    ylim([0 max([1.25*max(transfer_vec) 2.25])]);
     
     % get current plot axes, set font and line width
     set(gca,'FontSize',22,'LineWidth',2);
-    set(gcf, 'units', 'normalized'); % set plot sizing to normalized units
+    % set(gcf, 'units', 'normalized'); % set plot sizing to normalized units
     
     % set position of figure on screen [distance from left, top, width, height]
     set(gcf, 'Position', [0.1, 0.1, .6, 0.8]);
@@ -194,7 +223,7 @@ clear s_read; % close serial connection by deleting object
 
 %% Save data
 filename = sprintf('MAE170_lab4_part1_%s',datetime('now','Format',"yyyy-MM-dd-HH-mm-ss"));
-% Be sure to change filenames if you don’t want to overwrite your data!
+% Be sure to change filenames if you don't want to overwrite your data!
 save([filename, '.mat'], 'f_vec','transfer_vec'); 
 % save frequency and gain to mat file
 csvwrite([filename, '.csv'], ['f_vec','transfer_vec']);
